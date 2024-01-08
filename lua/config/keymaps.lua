abfb3aa2665ed1ec4b24ea07473c682e7bec67bf
Mhirii <ahmedmhiri218@gmail.com>
2024-01-08 17:26:39 +0100

    added harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -179,7 +179,11 @@ map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
--- extra
+--          ╭─────────────────────────────────────────────────────────╮
+--          │                          extra                          │
+--          ╰─────────────────────────────────────────────────────────╯
+
+-- lspsaga
     -- { "K", ":Lspsaga hover_doc ++quiet<cr>", desc = "Hover" },
     map("n", "gh", ":Lspsaga finder<cr>", {desc = "LSP finder" })
     -- { "gd", ":Lspsaga goto_definition<cr>", desc = "Goto definition" },
@@ -193,6 +197,39 @@ map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
     map("n", "<leader>ci", ":Lspsaga incoming_calls<cr>", {desc = "Incoming calls" })
     map("n", "<leader>co", ":Lspsaga outgoing_calls<cr>", {desc = "Outgoing calls" })
     map({ "n", "x" }, "<leader>ca", ":Lspsaga code_action<cr>", {desc = "Code action"})
+
+-- harpoon
+local harpoon = require("harpoon")
+harpoon:setup()
+map(
+  {"n","x"}, "<leader>ha",
+  function()
+      harpoon:list():append()
+    end,
+  {desc = "Harpoon Append"}
+)
+map(
+  {"n","x"}, "<leader>hm",
+  function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+    end,
+  {desc = "Harpoon Quick Menu"}
+)
+--TODO: add leader h[1,2,3,4...] 
+map(
+  {"n","x"}, "<leader>hp",
+  function()
+    harpoon:list():prev()
+    end,
+  {desc = "Harpoon Previous Mark"}
+)
+map(
+  {"n","x"}, "<leader>hn",
+  function()
+    harpoon:list():next()
+    end,
+  {desc = "Harpoon Next Mark"}
+)
 
 
 if vim.g.vscode then
